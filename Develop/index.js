6ffc24058158758669821f9863b0e085d9038392
oak45985 <oak45985@gmail.com>
2022-07-28 22:56:30 -0500

    final changes

--- Develop/index.js
+++ Develop/index.js
@@ -156,14 +156,3 @@ readMeData()
     .catch(err => {
         console.log(err);
     })
-// TODO: Create an array of questions for user input
-// const questions = [];
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
